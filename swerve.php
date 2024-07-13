07865a3ed8a0dca2adaaa0fd090ab4c1d2a40fae
Frode Børli <frode@ennerd.com>
2024-07-13 01:00:13 +0000

    Fix composer.json

--- swerve.php
+++ swerve.php
@@ -3,20 +3,11 @@
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Factory\AppFactory;
-use Swerve\Psr17\Psr17Factory;
 
 $app = AppFactory::create();
-
-//$app->addErrorMiddleware(true, true, true);
-
 $app->get('/', function (RequestInterface $request, ResponseInterface $response) {
-    $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
-    return $response;
-});
+    $response->getBody()->write('Hello, World');
 
-$app->get('/hello/{name}', function (RequestInterface $request, ResponseInterface $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
     return $response;
 });
 
